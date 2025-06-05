55be64a4e84f5ea8fcd476e046794da933969372
Heitor <heitordomingos98@hotmail.com>
2025-06-05 02:29:30 -0300

    Commit de atualização 2.0 da API de Cursos Node.js

--- server.js
+++ server.js
@@ -55,7 +55,4 @@ app.get('/gestao',(req,res)=>{
     res.json(gestao)
 })
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, ()=>{
-    console.log(`Servidor rodando na porta ${PORT}`)
-})
+module.exports = app;
